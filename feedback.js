325c063266b41b7cb1bf294525037a77a5293acd
Harry Lumb <harrylumbb@gmail.com>
2022-10-18 10:35:18 +0100

    welcome & feedback HTML/CSS

---
+++ feedback.js
@@ -0,0 +1,12 @@
+const paths = document.querySelectorAll("path")
+const stars = document.querySelectorAll('.svgs')
+    
+const starColor = function(){
+    for (let i = 0; i < stars.length; i++){
+        stars[i].addEventListener('click,' ()=)
+    }
+
+for (let i = 0; i < paths.length; ++i) {
+    paths[i].setAttribute('style', 'fill:green');
+}
+}
