e8503439eb8f91fca18e3cce86bb4084f26e76de
Harry Lumb <harrylumbb@gmail.com>
2022-10-18 11:17:22 +0100

    .

--- feedback.js
+++ feedback.js
@@ -1,14 +1,15 @@
-const paths = document.querySelectorAll("path")
-const stars = document.querySelectorAll('.svgs')
-    
-const starColor = function(){
-    for (let i = 0; i < stars.length; i++){
-        stars[i].addEventListener('click,' () => {
-            
-        })
-    }
+const paths = document.querySelectorAll("path");
+const stars = document.querySelectorAll(".svg");
 
-for (let i = 0; i < paths.length; ++i) {
-    paths[i].setAttribute('style', 'fill:green');
-}
-}
+const starColor = function () {
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener("click", () => {
+      for (let i = 0; i < paths.length; ++i) {}
+      paths[i].setAttribute('style', 'fill: #00ffff')
+    });
+    
+    
+  }
+};
+
+starColor();
